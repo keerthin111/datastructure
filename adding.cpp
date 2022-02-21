eeace373cd2836691de9ce73683cd4c144d4b223
keerthin111 <97940146+keerthin111@users.noreply.github.com>
2022-02-21 12:54:32 +0530

    Add files via upload

---
+++ adding.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i=0,n,sum=0;
+	cout<<"enter 10 number:\n";
+	while(i<10)
+	{
+	cin>>n;
+	sum=sum+n;
+	i++;
+    }
+ cout<<" the sum of 10 number:"<<sum;
+ return 0;
+}
