9dfa167915a784f206509be850124e297a8ea8ff
keerthin111 <97940146+keerthin111@users.noreply.github.com>
2022-02-21 10:37:29 +0530

    Add files via upload

---
+++ reverse.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[20],i,n;
+	cout<<"enter the size of an array:";
+	cin>>n;
+	cout<<"enter the element of an array:"<<endl;v
+	for(i=0;i<n;i++)
+	{
+	cin>>arr[i];
+    }
+	cout<<"Reversed elements are:"<<endl;
+    for(i=(n-1);i>=0;i--)
+	cout<<arr[i]<<" "<<endl;
+	return 0;	
+	}
+
